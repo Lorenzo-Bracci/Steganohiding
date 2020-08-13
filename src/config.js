c251ec5172b2f96df19bc6af4b84fb466502c394
Lorenzo Bracci <lorenzobracci98@hotmail.com>
2020-08-13 18:08:53 +0200

    add

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = global.config = {
     //REACT_APP_ROUTER_BASE: "/steganohiding",
-    serverHide: "http://localhost:9090/steganohiding/HideData",
-    serverReveal: "http://localhost:9090/steganohiding/RevealData" 
+    serverHide: "http://steganohiding-env.eba-pf5gfnue.us-west-2.elasticbeanstalk.com/HideData",
+    serverReveal: "http://steganohiding-env.eba-pf5gfnue.us-west-2.elasticbeanstalk.com//RevealData" 
 };
