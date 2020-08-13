4cd5fa209fd84beebe20ab1e497d7bc5178f30f8
Lorenzo Bracci <lorenzobracci98@hotmail.com>
2020-08-13 09:38:36 +0200

    Updates

---
+++ precache-manifest.dc25a88ecbc1742e8dc5734d3d04a4dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441200354158462068852f0b75aa67e6",
+    "url": "/Steganohiding/index.html"
+  },
+  {
+    "revision": "18eec55c9689c7cc6417",
+    "url": "/Steganohiding/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dcd939608116eefb8599",
+    "url": "/Steganohiding/static/css/main.0a177c9f.chunk.css"
+  },
+  {
+    "revision": "18eec55c9689c7cc6417",
+    "url": "/Steganohiding/static/js/2.e44579f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Steganohiding/static/js/2.e44579f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd939608116eefb8599",
+    "url": "/Steganohiding/static/js/main.88495def.chunk.js"
+  },
+  {
+    "revision": "ef90af31c3dbebf3799f",
+    "url": "/Steganohiding/static/js/runtime-main.7f1019e1.js"
+  },
+  {
+    "revision": "c925c8499bf59a8b214139968c5a6fae",
+    "url": "/Steganohiding/static/media/Lock_pending.c925c849.svg"
+  }
+]);
