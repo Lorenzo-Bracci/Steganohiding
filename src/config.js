83906c5fdcc91c18630ac94ca001df3de0af21e2
Lorenzo Bracci <lorenzobracci98@hotmail.com>
2020-08-15 11:18:07 +0200

    add

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = global.config = {
    // serverHide:  "http://localhost:9090/steganohiding/HideData",
   //  serverReveal: "http://localhost:9090/steganohiding/RevealData"
     serverHide: "http://steganohiding-env.eba-pf5gfnue.us-west-2.elasticbeanstalk.com/HideData",
-    serverReveal: "http://steganohiding-env.eba-pf5gfnue.us-west-2.elasticbeanstalk.com//RevealData" 
+    serverReveal: "http://steganohiding-env.eba-pf5gfnue.us-west-2.elasticbeanstalk.com/RevealData" 
 };
